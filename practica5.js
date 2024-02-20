90500c7ee8ee090254ea9a077120949c0b254779
Rodrigo Medrano <rodrigo.medranoorta@umg.edu.mx>
2024-02-19 19:32:58 -0600

    19/02/24, Práctica 5 - Sesiones y Cookies, Rodrigo Medrano

--- practica5.js
+++ practica5.js
@@ -12,23 +12,46 @@ app.use(session({
 
 app.use((req, res, next) => {
     //capturamos la ruta actual
+    if (req.session.visitados){
+        req.session.visitados={}
+    }
+    const rutaActual=req.path
+    req.session.visitados[rutaActual]=req.session.visitados[rutaActual]||0
+    req.session.visitados[rutaActual]++
+    
     //inicializamos el valor de la key que apunta a ruta actual con el valor actual o con cero
     //incrementamos el valor de la key que apunta a la ruta actual
     //si el contador llega a incrementarse a 3
         //escribimos el mensaje
         //en una respuesta de texto enviamos un script javascript concatena
-    //si aun no es tres... continuamos la ejecucion de las rutas
-    next()
-}
-
-);
+    if (req.session.visitados[rutaActual] === 3) {
+        res.send(`<script>alert('Has visitado esta página 3 veces');</script>`);
+    }
+    else {
+        next();
+    }
+});
 
 app.get('/', (req, res) => {
-    res.send('Visita las paginas que tu quieras');
+    const userAgent = req.headers['user-agent'];
+    res.send(`<p>Tu navegador es: ${userAgent}</p>`);
 });
+
 app.get('/query', (req, res) => {
+    const query_user=req.query
+    if(query_user.dato1){
+        res.cookie("cookie1" , query_user.dato1,{maxAge:1000})
+    }
+    if(query_user.dato2){
+        res.cookie("cookie2" , query_user.dato2,{maxAge:1000})
+    }
+    if(query_user.dato3){
+        res.cookie("cookie3" , query_user.dato3,{maxAge:1000})
+    }
+    
     res.send('querys');
 });
+
 app.get('/ruta1', (req, res) => {
     res.send('Pagina de ruta1');
 });
@@ -36,6 +59,7 @@ app.get('/ruta1', (req, res) => {
 app.get('/ruta2', (req, res) => {
     res.send('Pagina de ruta2');
 });
+
 app.get('/ruta3', (req, res) => {
     res.send('Pagina de ruta3');
 });
@@ -43,6 +67,7 @@ app.get('/ruta3', (req, res) => {
 app.get('/ruta4', (req, res) => {
     res.send('Pagina de ruta4');
 });
+
 app.get('/historial', (req, res) => {
     let paginas = null
     res.send(`Páginas consultadas: ${paginas}`);
--- sesionsimple.js
+++ sesionsimple.js
@@ -3,6 +3,17 @@ const session = require('express-session')
 const port = 3500
 const app = express()
 
+app.use(session({
+    secret: 'grupocibernetica4',
+    resave: true,
+    savUninitialized: true,
+}));
+
+app.get('/', (req, res) => {
+    req.session.conteo = req.session ? req.session.conteo + 1 : 1;
+    res.send(`Ruta visitada ${req.session.conteo} veces`)
+})
+
 app.get('/', (req, res) => {
     let veces=0
     res.send(`Ruta visitada ${veces} veces`)
