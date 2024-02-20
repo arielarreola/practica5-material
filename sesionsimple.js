f7a28d3df78b2405dc59dce9db27b4e609c7ba8e
valemonteverde17 <valeria.banuelosmonteverde@umg.edu.mx>
2024-02-19 23:40:50 -0600

    19/02/2024 - Práctica 5 - VBM

--- sesionsimple.js
+++ sesionsimple.js
@@ -3,10 +3,18 @@ const session = require('express-session')
 const port = 3500
 const app = express()
 
+app.use(session({
+    secret: 'aleatoria',
+    saveUninitialized: true,
+    resave: true,
+    cookie:{
+        masAge: 50000
+    }
+}))
+
 app.get('/', (req, res) => {
-    let veces=0
-    res.send(`Ruta visitada ${veces} veces`)
-    veces++//variable que no va a funcionar y nunca se actualizará
+    req.session.visitados=req.session.visitados?req.session.visitados+1:1
+    res.send(`Ruta visitada ${req.session.visitados} veces`)
 })
 
 
