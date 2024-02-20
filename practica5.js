9083958d7dcd34502a948f80c88aa33e16c7fe52
mario <marioarturo.gaytanorozco@umg.edu.mx>
2024-02-19 22:09:40 -0600

    19/02/2024 Practica 5, mi tarea de MARIO GAYTAN

--- practica5.js
+++ practica5.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const session = require('express-session');
+const cookieParser = require('cookie-parser')
 const port = 3700;
 const app = express();
+app.use(cookieParser())
+
 
 app.use(session({
     user: 'admin',
@@ -9,8 +12,21 @@ app.use(session({
     resave: false,
     saveUninitialized: true,
 }));
-
+// Middleware para rastrear visitas a cada ruta
 app.use((req, res, next) => {
+    if(!req.session.visitados){
+        req.session.visitados={}
+    }
+    const rutaActual = req.path
+    req.session.visitados[rutaActual] = req.session.visitados[rutaActual]||0
+    req.session.visitados[rutaActual]++
+    if(req.session.visitados[rutaActual]===3){
+        const msg = `Parece que te interesa la ruta ${rutaActual}`
+        res.status(200).send(`<script>alert('${msg}')</script>`)
+    }else{
+        next()
+    }
+
     //capturamos la ruta actual
     //inicializamos el valor de la key que apunta a ruta actual con el valor actual o con cero
     //incrementamos el valor de la key que apunta a la ruta actual
@@ -18,34 +34,62 @@ app.use((req, res, next) => {
         //escribimos el mensaje
         //en una respuesta de texto enviamos un script javascript concatena
     //si aun no es tres... continuamos la ejecucion de las rutas
-    next()
+    
 }
 
 );
 
 app.get('/', (req, res) => {
-    res.send('Visita las paginas que tu quieras');
+    const agent=req.headers['user-agent']
+    res.cookie("navegador",agent,{
+        httpOnly:true
+    })
+    res.send('Prueba las paginas que tu quieras, son frutas, Manzana, Pera, Sandia, Mango y Fruta');
 });
+// Ruta GET con query para capturar tres datos y guardar cada uno en una cookie diferente
 app.get('/query', (req, res) => {
-    res.send('querys');
+    const query_user=req.query
+    if(query_user.data1){
+        res.cookie("Data1",query_user.data1,{
+            httpOnly:true,
+            maxAge:1000*10
+        })
+    }
+    if(query_user.data2){
+        res.cookie("Data2",query_user.data2,{
+            httpOnly:true,
+            maxAge:1000*30
+        })
+    }
+    if(query_user.data3){
+        res.cookie("Data3",query_user.data3,{
+            httpOnly:true,
+            maxAge:1000*20
+        })
+    }
+
+    res.send('Datos guardados en cookies');
 });
-app.get('/ruta1', (req, res) => {
-    res.send('Pagina de ruta1');
+app.get('/Mango', (req, res) => {
+    res.send('Pagina de Mango');
 });
 
-app.get('/ruta2', (req, res) => {
-    res.send('Pagina de ruta2');
+app.get('/Pera', (req, res) => {
+    res.send('Pagina de Pera');
 });
-app.get('/ruta3', (req, res) => {
-    res.send('Pagina de ruta3');
+app.get('/Manzana', (req, res) => {
+    res.send('Pagina de Manzana');
 });
 
-app.get('/ruta4', (req, res) => {
-    res.send('Pagina de ruta4');
+app.get('/Sandia', (req, res) => {
+    res.send('Pagina de Sandia');
+});
+app.get('/Fruta', (req, res) => {
+    res.send('Pagina de Fruta');
 });
 app.get('/historial', (req, res) => {
-    let paginas = null
-    res.send(`Páginas consultadas: ${paginas}`);
+    const paginas = req.session.visitados
+    res.send(`Páginas consultadas: ${JSON.stringify(paginas)}`);
 });
 
 app.listen(port, () => {
